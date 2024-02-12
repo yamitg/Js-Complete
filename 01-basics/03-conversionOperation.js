aab3adf7139b5174ef2a4448bdb0345cfe5dd92d
yamitg <135598586+yamitg@users.noreply.github.com>
2024-02-12 08:00:19 +0000

    container config done

--- 01-basics/03-conversionOperation.js
+++ 01-basics/03-conversionOperation.js
@@ -1,3 +1,5 @@
 let value =3;
 let test=-value;
 console.log(test)
+
+console.log(2+2+2);
---
+++ 01-basics/05-Strings.js
@@ -0,0 +1,12 @@
+const name = "Amit kumar ";
+const repoCount = 50;
+
+console.log(name + repoCount + " value");
+
+console.log(`My name is name ${name}and my repo count is ${repoCount}`);
+
+const gameNAme = new String("Amit Yadav");
+console.log(gameNAme);
+console.log(gameNAme.length);
+console.log(gameNAme.split('-'));
+console.log(gameNAme.charAt(2));
