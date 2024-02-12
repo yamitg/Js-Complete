b7ca431ab3ec59d3661f56f5d78f1a81ff9467d3
yamitg <135598586+yamitg@users.noreply.github.com>
2024-02-12 06:12:29 +0000

    container config done

---
+++ 01-basics/01-test_let_const.js
@@ -0,0 +1,15 @@
+console.log("Amit Kumarr");
+
+const accountemail="amyrao@gmail.com"
+let accountId = "101"
+var accountpwd = "854940987"
+// account city = "MAhendragarh"
+
+
+accountId = "201"
+accountpwd = "99999999"
+// account city = "Rewari"
+
+
+console.table([accountId, accountemail, accountpwd])
+
---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,14 @@
+console.log(33 + 3+"4");
+
+console.log("Amit kumar");
+
+
+let isEmpty = false;
+console.log(isEmpty);
+
+let amit;
+console.log(amit);
+let kumar = null;
+console.table([amit,kumar])
+
+
