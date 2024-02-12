11d02677c6a06a5a1695be3aa62043828c36da79
yamitg <135598586+yamitg@users.noreply.github.com>
2024-02-12 06:51:02 +0000

    container config done

--- 01-basics/02-datatypes.js
+++ 01-basics/02-datatypes.js
@@ -7,7 +7,7 @@ let isEmpty = false;
 console.log(isEmpty);
 
 let amit;
-console.log(amit);
+console.log( typeof "a");
 let kumar = null;
 console.table([amit,kumar])
 
---
+++ 01-basics/03-conversionOperation.js
@@ -0,0 +1,3 @@
+let value =3;
+let test=-value;
+console.log(test);
